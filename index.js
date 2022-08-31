0f1f2e1aa61775f89cab3c92f16867916be36f0b
Steven Rosario <strosariogarcia@gmail.com>
2022-08-31 12:18:53 -0400

    commit tree 58c9ead6394df555071db9f20bd657c89c9d5c8b

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM has been loaded!")
+    const change = document.querySelector('#text')
+    change.textContent = "This is really cool!"
+})
+
